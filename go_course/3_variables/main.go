86121e2c5b4324723c90d3358c090860442a87d6
Snehal-Pradhan <snehalpradhan23@gmail.com>
2025-07-04 05:36:34 +0530

    variables in go

---
+++ go_course/3_variables/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string = "golang"
+	var name = "golang"
+	var isAdult = true
+	var age int = 30
+	gender := "male"
+	fmt.Println(name)
+	fmt.Println(isAdult)
+	fmt.Println(age)
+	fmt.Println(gender)
+}
