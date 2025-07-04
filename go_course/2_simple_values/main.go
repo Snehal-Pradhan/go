5094fe203bb49a195039b2c1a91d76beafe6e04f
Snehal-Pradhan <snehalpradhan23@gmail.com>
2025-07-04 05:36:17 +0530

    datatypes in go

---
+++ go_course/2_simple_values/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(true)
+	fmt.Println(false)
+	fmt.Println("hello-world")
+	fmt.Println(156)
+	fmt.Println(156.123)
+	fmt.Println(156.1 / 7.165)
+}
